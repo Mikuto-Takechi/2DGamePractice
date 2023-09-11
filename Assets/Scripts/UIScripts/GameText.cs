2a675a3965e5bcf4701ddc237d9fd8f5dc82ab29
Mikuto <134473854+Mikuto-Takechi@users.noreply.github.com>
2023-09-11 21:20:10 +0900

    [update]ステージボタン関連のスクリプトを1つにまとめた。

--- Assets/Scripts/UIScripts/GameText.cs
+++ Assets/Scripts/UIScripts/GameText.cs
@@ -8,7 +8,6 @@ public class GameText : MonoBehaviour
     Text _text;
     [SerializeField] string[] _texts;
     [SerializeField] TextType _type = TextType.None;
-    [Tooltip("ステージ名の指定（_textTypeがStageRecordになっていないと設定しても意味がない）"), SerializeField] string _stageName = "";
     int _indexNumber = 0;
     void Start()
     {
@@ -22,32 +21,6 @@ public class GameText : MonoBehaviour
         if (_type == TextType.Time) _text.text = $"時間 {GameManager.instance._stageTime.ToString("F2")}";
         if (_type == TextType.ClearSteps) _text.text = GameManager.instance._stepText;
         if (_type == TextType.ClearTime) _text.text = GameManager.instance._timeText;
-        if (_type == TextType.TimeRecord)
-        {
-            var times = GameManager.instance._timeRecords;
-            if (times == null) return;
-            if(times.ContainsKey(_stageName) && times[_stageName] != default)
-            {
-                _text.text = $"時間：{times[_stageName].ToString("F2")}";
-            }
-            else
-            {
-                _text.text = "- - -";
-            }
-        }
-        if(_type == TextType.StepRecord)
-        {
-            var steps = GameManager.instance._stepsRecords;
-            if (steps == null) return;
-            if (steps.ContainsKey(_stageName) && steps[_stageName] != default)
-            {
-                _text.text = $"歩数：{steps[_stageName]}";
-            }
-            else
-            {
-                _text.text = "- - -";
-            }
-        }
     }
     IEnumerator MoveText()
     {
--- Assets/Scripts/UIScripts/StageButton.cs
+++ Assets/Scripts/UIScripts/StageButton.cs
@@ -2,23 +2,47 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
-
+/// <summary>
+/// ステージ選択ボタンに付けるコンポーネント
+/// </summary>
+[RequireComponent(typeof(Button))]
 public class StageButton : MonoBehaviour
 {
+    /// <summary>そのボタンに割り当てるステージ名</summary>
+    [SerializeField] string _stageName = "";
+    /// <summary>ステージ解放関連の処理を行うかどうか</summary>
+    [SerializeField] bool _enableLockProcess = true;
     /// <summary>クリア時間記録を表示させるテキスト</summary>
     [SerializeField] Text _timeRecord = null;
     /// <summary>歩数記録を表示させるテキスト</summary>
     [SerializeField] Text _stepRecord = null;
-    /// <summary>そのボタンに割り当てるステージ名</summary>
-    [SerializeField] string _stageName = "";
+    /// <summary>ロックされた時に表示するオブジェクト</summary>
+    [SerializeField] GameObject _lockImage = null;
+    /// <summary>ロックされた時に非表示するオブジェクト</summary>
+    [SerializeField] GameObject[] _hiddenObjects = null;
+    Button _button;
+    private void Start()
+    {
+        _button = GetComponent<Button>();
+        if (_lockImage) _lockImage.SetActive(false);
+    }
     private void Update()
     {
-        Record(GameManager.instance._timeRecords, _timeRecord);
-        Record(GameManager.instance._stepsRecords, _stepRecord);
+        StageRecord(GameManager.instance._timeRecords, _timeRecord);
+        StageRecord(GameManager.instance._stepsRecords, _stepRecord);
+        if (_enableLockProcess == false) return;
+        StageLock();
     }
-    void Record<T>(Dictionary<string, T> dic, Text text) where T : IComparable<T>, IFormattable
+    /// <summary>
+    /// ステージ記録をテキストに表示させる
+    /// </summary>
+    /// <typeparam name="T">記録の型</typeparam>
+    /// <param name="dic">ステージ記録の入ったDictionary</param>
+    /// <param name="text">表示用テキスト</param>
+    void StageRecord<T>(Dictionary<string, T> dic, Text text) where T : IComparable<T>, IFormattable
     {
         if (dic == null) return;
         if (text == null) return;
@@ -34,6 +58,25 @@ public class StageButton : MonoBehaviour
         else
         {
             text.text = "- - -";
+        }
+    }
+    /// <summary>
+    /// ステージが解放されているかを判断してオンオフを切り替える
+    /// </summary>
+    void StageLock()
+    {
+        if (_button.IsInteractable())//ボタンが干渉可能ならば
+        {
+            if (GameManager.instance._unlockStages.Contains(_stageName))//ステージが解放されているかを確認する
+            {
+                _button.interactable = true;
+            }
+            else
+            {
+                _button.interactable = false;
+            }
+            _hiddenObjects.Where(o => o).ToList().ForEach(o => o.SetActive(_button.IsInteractable()));
+            if (_lockImage) _lockImage.SetActive(!_button.IsInteractable());
         }
     }
 }
